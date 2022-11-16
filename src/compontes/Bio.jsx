3796331723c91b90865d5fd767a106db8cd6c980
marta-santos <martasilvaa325@gmail.com>
2022-11-16 16:21:06 -0300

    mudanças na bio em algumas palavras

--- src/compontes/Bio.jsx
+++ src/compontes/Bio.jsx
@@ -3,8 +3,8 @@ function Bio() {
         <div>
             <h1>Meu <span>github</span> search</h1>
             <br />
-            <h2>Olá Muito prazer, eu sou a <span>Marta</span>! </h2>
-            <p>Me aprensentando um pouquinho, eu sou a <span>Marta</span>, tenho 25 anos. Atualmente, sou aluna de Front end na <span>Reprograma</span> e está sendo uma experiência incrível na minha vida. <br /> O projeto tem mudado a minha vida de formas inimanigináveis. Antes de entrar aqui na <span>Reprograma</span> , não sabia nem como ligar um computador direito. Agora,estou aqui desenvolvendo meu segundo projeto em <span> React </span>!!</p>
+            <h2>Olá! Muito prazer, eu sou a <span>Marta</span>! </h2>
+            <p>Me aprensentando um pouquinho, tenho 25 anos atualmente, sou aluna de Front end na <span>Reprograma</span> e está sendo uma experiência incrível na minha vida. <br /> O projeto tem mudado a minha vida de formas inimanigináveis. <br /> Antes de entrar aqui na <span>Reprograma</span> , não sabia nem como ligar um computador direito e, agora, estou aqui desenvolvendo meu segundo projeto em <span> React </span>!!</p>
             <img src="https://avatars.githubusercontent.com/u/109400186?v=4" alt="mulher negra sorrindo ela tem cabelos medios e esta maquiada" />
         </div>
     )
