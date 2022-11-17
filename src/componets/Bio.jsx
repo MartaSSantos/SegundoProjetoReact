a1d3d1e690910ade6e2f2670153324778946589c
marta-santos <martasilvaa325@gmail.com>
2022-11-17 13:01:48 -0300

    melhorando a descrição da foto

--- src/componets/Bio.jsx
+++ src/componets/Bio.jsx
@@ -5,7 +5,7 @@ function Bio() {
             <br />
             <h2>Olá! Muito prazer, eu sou a <span>Marta</span>! </h2>
             <p>Me aprensentando um pouquinho, tenho 25 anos atualmente, sou aluna de Front end na <span>Reprograma</span> e está sendo uma experiência incrível na minha vida. <br /> O projeto tem mudado a minha vida de formas inimanigináveis. <br /> Antes de entrar aqui na <span>Reprograma</span> , não sabia nem como ligar um computador direito e, agora, estou aqui desenvolvendo meu segundo projeto em <span> React </span>!!</p>
-            <img src="https://avatars.githubusercontent.com/u/109400186?v=4" alt="mulher negra sorrindo ela tem cabelos castanho, comprimento de cabelo  medio, e esta maquiada" />
+            <img src="https://avatars.githubusercontent.com/u/109400186?v=4" alt="mulher negra sorrindo ela tem cabelos castanho escuro com as ponas claras, comprimento de cabelo  medio, e esta maquiada" />
         </div>
     )
 }
